db114581a37bbbda1eca89968b3163c67517dcce
Clementine.Iradukunda <clementineiradu365@gmail.com>
2025-04-05 14:16:05 +0200

    now

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const booksRoutes = require('./routes/books');//this will import the books route
-
-
-
-
-// Routes
-app.use('/books', booksRoutes); 
